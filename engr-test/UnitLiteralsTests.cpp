331d5ff0ead04e43b7dbafa082ee470009b64001
Bryson Tanner <sandwichman1618@gmail.com>
2023-02-19 09:22:19 -0600

    test: Add tests for Kelvin, Ampere, Mole, and Candela

--- engr-test/UnitLiteralsTests.cpp
+++ engr-test/UnitLiteralsTests.cpp
@@ -4,7 +4,7 @@
 
 using namespace eng::units::metric;
 
-TEST(TestLiteralsMetric, SecondLiteral) {
+TEST(TestLiteralsMetric, Second) {
   eng::SIUnit<eng::TimeT(1)> expected{23.5};
 
   auto actual = 23.5 * s;
@@ -13,7 +13,7 @@ TEST(TestLiteralsMetric, SecondLiteral) {
   EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
 }
 
-TEST(TestLiteralsMetric, KilogramLiteral) {
+TEST(TestLiteralsMetric, Kilogram) {
   eng::SIUnit<eng::MassT(1)> expected{-4};
 
   auto actual = -4 * kg;
@@ -22,10 +22,46 @@ TEST(TestLiteralsMetric, KilogramLiteral) {
   EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
 }
 
-TEST(TestLiteralsMetric, MeterLiteral) {
+TEST(TestLiteralsMetric, Meter) {
   eng::SIUnit<eng::LengthT(1)> expected{2};
-  
+
   auto actual = 2 * m;
+
+  EXPECT_EQ(expected.unit, actual.unit);
+  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
+}
+
+TEST(TestLiteralsMetric, Kelvin) {
+  eng::SIUnit<eng::TemperatureT(1)> expected{810.7};
+
+  auto actual = 810.7 * K;
+
+  EXPECT_EQ(expected.unit, actual.unit);
+  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
+}
+
+TEST(TestLiteralsMetric, Ampere) {
+  eng::SIUnit<eng::CurrentT(1)> expected{0.567};
+
+  auto actual = 0.567 * A;
+
+  EXPECT_EQ(expected.unit, actual.unit);
+  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
+}
+
+TEST(TestLiteralsMetric, Mole) {
+  eng::SIUnit<eng::AmountT(1)> expected{56.123};
+
+  auto actual = 56.123 * mol;
+
+  EXPECT_EQ(expected.unit, actual.unit);
+  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
+}
+
+TEST(TestLiteralsMetric, Candela) {
+  eng::SIUnit<eng::LuminosityT(1)> expected{4};
+
+  auto actual = 4 * cd;
 
   EXPECT_EQ(expected.unit, actual.unit);
   EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
