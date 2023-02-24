0445e248d5d08f873620ac7045d06d2e1a1c3baf
Bryson Tanner <sandwichman1618@gmail.com>
2023-02-23 20:01:38 -0600

    refactor: Create a macro to simplify creating lots of unit type tests

--- engr-test/UnitTypesTests.cpp
+++ engr-test/UnitTypesTests.cpp
@@ -3,58 +3,17 @@
 #include <units/SIDimension.h>
 #include <units/UnitTypes.h>
 
-TEST(TestUnitTypes, Time) {
-  eng::SIDimension expected = eng::TimeT(1);
+#define UNIT_TYPE_TEST(Literal, equivalentUnit) \
+  TEST(TestUnitTypes, Literal) {                   \
+    eng::SIDimension expected = (equivalentUnit);  \
+    eng::##Literal  actual{0};                        \
+    EXPECT_EQ(expected, actual.unit);           \
+  }
 
-  eng::Time actual{0};
-
-  EXPECT_EQ(expected, actual.unit);
-}
-
-TEST(TestUnitTypes, Mass) {
-  eng::SIDimension expected = eng::MassT(1);
-
-  eng::Mass actual{0};
-
-  EXPECT_EQ(expected, actual.unit);
-}
-
-TEST(TestUnitTypes, Length) {
-  eng::SIDimension expected = eng::LengthT(1);
-
-  eng::Length actual{0};
-
-  EXPECT_EQ(expected, actual.unit);
-}
-
-TEST(TestUnitTypes, Temperature) {
-  eng::SIDimension expected = eng::TemperatureT(1);
-
-  eng::Temperature actual{0};
-
-  EXPECT_EQ(expected, actual.unit);
-}
-
-TEST(TestUnitTypes, Current) {
-  eng::SIDimension expected = eng::CurrentT(1);
-
-  eng::Current actual{0};
-
-  EXPECT_EQ(expected, actual.unit);
-}
-
-TEST(TestUnitTypes, Amount) {
-  eng::SIDimension expected = eng::AmountT(1);
-
-  eng::Amount actual{0};
-
-  EXPECT_EQ(expected, actual.unit);
-}
-
-TEST(TestUnitTypes, Luminosity) {
-  eng::SIDimension expected = eng::LuminosityT(1);
-
-  eng::Luminosity actual{0};
-
-  EXPECT_EQ(expected, actual.unit);
-}
+UNIT_TYPE_TEST(Time, eng::TimeT(1))
+UNIT_TYPE_TEST(Mass, eng::MassT(1))
+UNIT_TYPE_TEST(Length, eng::LengthT(1))
+UNIT_TYPE_TEST(Temperature, eng::TemperatureT(1))
+UNIT_TYPE_TEST(Current, eng::CurrentT(1))
+UNIT_TYPE_TEST(Amount, eng::AmountT(1))
+UNIT_TYPE_TEST(Luminosity, eng::LuminosityT(1))
