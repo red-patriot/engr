825cc3c7d1680dd7019bf31204b2bac7083c25ff
Bryson Tanner <sandwichman1618@gmail.com>
2023-02-25 15:24:39 -0600

    refactor: create macro to reduce duplication of test code

--- engr-test/MetricLiteralsTests.cpp
+++ engr-test/MetricLiteralsTests.cpp
@@ -3,119 +3,40 @@
 #include <units/metric.h>
 
 using namespace eng::units;
+using eng::invert;
+using eng::pow;
 
-TEST(TestLiteralsMetric, Second) {
-  eng::SIUnit<eng::TimeT(1)> expected{23.5};
+#define METRIC_LITERALS_TEST(name, expectedType, expectedValue, actualValue) \
+  TEST(TestLiteralsMetric, name) {                                           \
+    expectedType expected{expectedValue};                                    \
+    auto actual = actualValue;                                               \
+    EXPECT_EQ(expected.unit, actual.unit);                                   \
+    EXPECT_NEAR(expected.base(), actual.base(), 1e-10);                      \
+  }
 
-  auto actual = 23.5 * s;
+// Base unit literals tests
+METRIC_LITERALS_TEST(Second, eng::SIUnit<eng::TimeT(1)>, 23.5, 23.5 * s)
+METRIC_LITERALS_TEST(Kilogram, eng::SIUnit<eng::MassT(1)>, -4, -4 * kg)
+METRIC_LITERALS_TEST(Meter, eng::SIUnit<eng::LengthT(1)>, 2, 2 * m)
+METRIC_LITERALS_TEST(Kelvin, eng::SIUnit<eng::TemperatureT(1)>, 810.7, 810.7 * K)
+METRIC_LITERALS_TEST(Ampere, eng::SIUnit<eng::CurrentT(1)>, 0.567, 0.567 * A)
+METRIC_LITERALS_TEST(Mole, eng::SIUnit<eng::AmountT(1)>, 56.123, 56.123 * mol)
+METRIC_LITERALS_TEST(Candela, eng::SIUnit<eng::LuminosityT(1)>, 4, 4 * cd)
 
-  EXPECT_EQ(expected.unit, actual.unit);
-  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
-}
+// Arithmetic combinations tests
+METRIC_LITERALS_TEST(Frequency, eng::SIUnit<eng::TimeT(-1)>, 7.7, 7.7 / s)
+METRIC_LITERALS_TEST(Velocity, eng::SIUnit<eng::LengthT(1) - eng::TimeT(1)>, 4, 4 * m / s)
+METRIC_LITERALS_TEST(Powers,
+                     eng::SIUnit<eng::MassT(1) + eng::LengthT(2) - eng::TimeT(3) - eng::CurrentT(2)>,
+                     300,
+                     300 * kg * pow<2>(m) * pow<-3>(s) * pow<-2>(A))
+METRIC_LITERALS_TEST(Roots,
+                     eng::SIUnit<eng::MassT({1, 2}) + eng::TimeT({-3, 5}) - eng::LuminosityT({1, 5})>,
+                     12.6,
+                     12.6 * (rt<2>(kg) * pow<-3, 5>(s) * rt<-5>(cd)))
+METRIC_LITERALS_TEST(Exponentiation,
+                     eng::SIUnit<eng::MassT({1, 3}) + eng::TimeT({3, 2}) + eng::CurrentT({-1, 2})>,
+                     4.8,
+                     (pow<1, 3>(27 * kg) * pow<3, 2>(4 * s) / rt<2>(25 * A)))
 
-TEST(TestLiteralsMetric, Kilogram) {
-  eng::SIUnit<eng::MassT(1)> expected{-4};
 
-  auto actual = -4 * kg;
-
-  EXPECT_EQ(expected.unit, actual.unit);
-  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
-}
-
-TEST(TestLiteralsMetric, Meter) {
-  eng::SIUnit<eng::LengthT(1)> expected{2};
-
-  auto actual = 2 * m;
-
-  EXPECT_EQ(expected.unit, actual.unit);
-  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
-}
-
-TEST(TestLiteralsMetric, Kelvin) {
-  eng::SIUnit<eng::TemperatureT(1)> expected{810.7};
-
-  auto actual = 810.7 * K;
-
-  EXPECT_EQ(expected.unit, actual.unit);
-  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
-}
-
-TEST(TestLiteralsMetric, Ampere) {
-  eng::SIUnit<eng::CurrentT(1)> expected{0.567};
-
-  auto actual = 0.567 * A;
-
-  EXPECT_EQ(expected.unit, actual.unit);
-  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
-}
-
-TEST(TestLiteralsMetric, Mole) {
-  eng::SIUnit<eng::AmountT(1)> expected{56.123};
-
-  auto actual = 56.123 * mol;
-
-  EXPECT_EQ(expected.unit, actual.unit);
-  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
-}
-
-TEST(TestLiteralsMetric, Candela) {
-  eng::SIUnit<eng::LuminosityT(1)> expected{4};
-
-  auto actual = 4 * cd;
-
-  EXPECT_EQ(expected.unit, actual.unit);
-  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
-}
-
-TEST(TestLiteralsMetric, Frequency) {
-  eng::SIUnit<eng::TimeT(-1)> expected{7.7};
-
-  auto actual = 7.7 / s;
-
-  EXPECT_EQ(expected.unit, actual.unit);
-  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
-}
-
-TEST(TestLiteralsMetric, Velocity) {
-  eng::SIUnit<eng::LengthT(1) + eng::TimeT(-1)> expected{4};
-
-  auto actual = 4 * m / s;
-
-  EXPECT_EQ(expected.unit, actual.unit);
-  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
-}
-
-TEST(TestLiteralsMetric, Resistance) {
-  eng::SIUnit<eng::MassT(1) + eng::LengthT(2) - eng::TimeT(3) - eng::CurrentT(2)> expected{300};
-
-  using eng::pow;
-
-  auto actual = 300 * kg * pow<2>(m) * pow<-3>(s) * pow<-2>(A);
-
-  EXPECT_EQ(expected.unit, actual.unit);
-  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
-}
-
-TEST(TestLiteralsMetric, Roots) {
-  eng::SIUnit<eng::MassT({1, 2}) + eng::TimeT({-3, 5}) - eng::LuminosityT({1, 5})> expected{12.6};
-
-  using eng::invert;
-  using eng::pow;
-
-  auto actual = 12.6 * rt<2>(kg) * pow<-3, 5>(s) * rt<-5>(cd);
-
-  EXPECT_EQ(expected.unit, actual.unit);
-  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
-}
-
-TEST(TestLiteralsMetric, Exponentiation) {
-  eng::SIUnit<eng::MassT({1, 3}) + eng::TimeT({3, 2}) + eng::CurrentT({-1, 2})> expected{4.8};
-
-  using eng::pow;
-  using eng::rt;
-
-  auto actual = pow<1, 3>(27 * kg) * pow<3, 2>(4 * s) / rt<2>(25 * A);
-
-  EXPECT_EQ(expected.unit, actual.unit);
-  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
-}
