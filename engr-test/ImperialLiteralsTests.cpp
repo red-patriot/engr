11f5677f3a53cc9d31ec2fbdc5aa3b0eab242037
Bryson Tanner <sandwichman1618@gmail.com>
2023-02-26 09:44:55 -0600

    refactor: Create test macro to reduce duplicated code in Imperial Literals tests

--- engr-test/ImperialLiteralsTests.cpp
+++ engr-test/ImperialLiteralsTests.cpp
@@ -4,83 +4,24 @@
 
 using namespace eng::units;
 
-TEST(TestLiteralsImperial, Second) {
-  eng::Time expected{23.5};
+#define IMPERIAL_LITERALS_TEST(name, expectedType, expectedValue, actualValue) \
+  TEST(TestLiteralsImperial, name) {                                         \
+    expectedType expected{expectedValue};                                      \
+    auto actual = actualValue;                                                 \
+    EXPECT_EQ(expected.unit, actual.unit);                                     \
+    EXPECT_NEAR(expected.base(), actual.base(), 1e-10);                        \
+  }
 
-  auto actual = 23.5 * s;
+// Base unit literals tests
+IMPERIAL_LITERALS_TEST(Second, eng::Time, 23.5, 23.5 * s)
+IMPERIAL_LITERALS_TEST(PoundMass, eng::Mass, 0.45359237, 1 * lbm)
+IMPERIAL_LITERALS_TEST(Yard, eng::Length, 0.9144, 1 * yd)
+IMPERIAL_LITERALS_TEST(Rankine, eng::Temperature, 30, 54 * R)
+IMPERIAL_LITERALS_TEST(Ampere, eng::Current, 0.567, 0.567 * A)
+IMPERIAL_LITERALS_TEST(Mole, eng::Amount, 56.123, 56.123 * mol)
+IMPERIAL_LITERALS_TEST(Candela, eng::Luminosity, 4, 4 * cd)
 
-  EXPECT_EQ(expected.unit, actual.unit);
-  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
-}
-
-TEST(TestLiteralsImperial, PoundMass) {
-  eng::Mass expected{0.45359237};
-
-  auto actual = 1 * lbm;
-
-  EXPECT_EQ(expected.unit, actual.unit);
-  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
-}
-
-TEST(TestLiteralsImperial, Yard) {
-  eng::Length expected{0.9144};
-
-  auto actual = 1 * yd;
-
-  EXPECT_EQ(expected.unit, actual.unit);
-  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
-}
-
-TEST(TestLiteralsImperial, Rankine) {
-  eng::Temperature expected{30};
-
-  auto actual = 54 * R;
-
-  EXPECT_EQ(expected.unit, actual.unit);
-  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
-}
-
-TEST(TestLiteralsImperial, Ampere) {
-  eng::Current expected{0.567};
-
-  auto actual = 0.567 * A;
-
-  EXPECT_EQ(expected.unit, actual.unit);
-  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
-}
-
-TEST(TestLiteralsImperial, Mole) {
-  eng::Amount expected{56.123};
-
-  auto actual = 56.123 * mol;
-
-  EXPECT_EQ(expected.unit, actual.unit);
-  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
-}
-
-TEST(TestLiteralsImperial, Candela) {
-  eng::Luminosity expected{4};
-
-  auto actual = 4 * cd;
-
-  EXPECT_EQ(expected.unit, actual.unit);
-  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
-}
-
-TEST(TestLiteralsImperial, Velocity) {
-  eng::Velocity expected{3.6576};
-
-  auto actual = 4 * yd / s;
-
-  EXPECT_EQ(expected.unit, actual.unit);
-  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
-}
-
-TEST(TestLiteralsImperial, Force) {
-  eng::Force expected{4.448'221'615'260'5};
-
-  auto actual = 1 * lbm * ((10.724'682'852'143'48) * yd) * pow<-2>(s);
-
-  EXPECT_EQ(expected.unit, actual.unit);
-  EXPECT_NEAR(expected.base(), actual.base(), 1e-10);
-}
+// Arithmetic combinations tests
+IMPERIAL_LITERALS_TEST(Velocity, eng::Velocity, 3.6576, 4 * yd / s)
+IMPERIAL_LITERALS_TEST(Powers, eng::Force, 4.448'221'615'260'5,
+                       1 * lbm * ((10.724'682'852'143'48) * yd) * pow<-2>(s))
