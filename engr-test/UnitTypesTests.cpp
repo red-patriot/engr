f5d00e55a1032bf9e96d9287a9c69c905545c705
Bryson Tanner <sandwichman1618@gmail.com>
2023-02-25 21:51:46 -0600

    test: Add tests for basic electrical units

--- engr-test/UnitTypesTests.cpp
+++ engr-test/UnitTypesTests.cpp
@@ -31,3 +31,10 @@ UNIT_TYPE_TEST(Jerk, eng::LengthT(1) - eng::TimeT(3))
 UNIT_TYPE_TEST(Force, eng::MassT(1) + eng::LengthT(1) - eng::TimeT(2))
 UNIT_TYPE_TEST(Energy, eng::MassT(1) + eng::LengthT(2) - eng::TimeT(2))
 UNIT_TYPE_TEST(Power, eng::MassT(1) + eng::LengthT(2) - eng::TimeT(3))
+
+UNIT_TYPE_TEST(Charge, eng::CurrentT(1) + eng::TimeT(1))
+UNIT_TYPE_TEST(Voltage, eng::MassT(1) + eng::LengthT(2) - eng::TimeT(3) - eng::CurrentT(1))
+UNIT_TYPE_TEST(Resistance, eng::MassT(1) + eng::LengthT(2) - eng::TimeT(3) - eng::CurrentT(2))
+UNIT_TYPE_TEST(Capacitance, eng::CurrentT(2) + eng::TimeT(4) - eng::MassT(1) - eng::LengthT(2))
+UNIT_TYPE_TEST(Inductance, eng::MassT(1) + eng::LengthT(2) - eng::TimeT(2) - eng::CurrentT(2))
+
