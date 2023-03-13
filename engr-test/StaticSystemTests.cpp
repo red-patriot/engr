5969431cda2cfbba805575a35c5e42ef94efaf45
Bryson Tanner <sandwichman1618@gmail.com>
2023-03-12 21:40:00 -0500

    test: Add test for simple 2D static system with only Forces

--- engr-test/StaticSystemTests.cpp
+++ engr-test/StaticSystemTests.cpp
@@ -12,18 +12,19 @@ TEST(TestStaticSystem, 2DSystemForceOnly) {
   engr::Force expectedTABC = 144.2 * N;
 
   engr::StaticSystem sys;
-  engr::Load actualR = engr::unknownForce2({1.2 / 3.7, 3.5 / 3.7},
-                                           Eigen::Vector2<engr::Length>::Zero());
-  engr::Load actualTABC = engr::unknownForce2(engr::unknownDirection,
-                                              Eigen::Vector2<engr::Length>::Zero());
+  engr::Load actualR = engr::unknownForce({1.2 / 3.7, 3.5 / 3.7, 0},
+                                          engr::Vector3<engr::Length>::Zero);
+  engr::Load actualTABC = engr::unknownForce(engr::unknownDirection,
+                                             engr::Vector3<engr::Length>::Zero);
 
-  sys.add(Eigen::Vector2<engr::Force>{0 * N, -400 * N},
-          Eigen::Vector2<engr::Length>::Zero());
+  sys.add(engr::Vector3<engr::Force>{0 * N, -400 * N, 0.0 * N},
+          engr::Vector3<engr::Length>::Zero);
   sys.add(actualR);
   sys.add(actualTABC);
 
-  sys.solve();
+  auto result = sys.solve();
 
+  EXPECT_TRUE(result);
   EXPECT_NEAR(expectedR.as(N), actualR.force().norm().as(N), 1e-10);
-  EXPECT_NEAR(expectedTABC.as(N), actualTABC.force.norm().as(N), 1e-10);
+  EXPECT_NEAR(expectedTABC.as(N), actualTABC.force().norm().as(N), 1e-10);
 }
